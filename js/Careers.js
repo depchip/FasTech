87a612e467640bd104702852ee6adedb3dca0fbf
AbdulshakoorAli <shakoormemon2@gmail.com>
2023-03-15 00:21:38 +0500

    json format added and rebuilt contact page

--- js/Careers.js
+++ js/Careers.js
@@ -1,57 +1,45 @@
-// SETTING UP CAREERS
+// // SETTING UP CAREERS
+let xhr = new XMLHttpRequest();
 
-const jobs = [
-  {
-    jobName: "Graphic Designer",
-    jobType: "Engineering",
-    jobCategory: "100% Remote",
-    jobApplyUrl: "https://forms.gle/WoiNgB4w4pjLa1bf9",
-  },
-  {
-    jobName: "Buisness Development Executive",
-    jobType: "Sales",
-    jobCategory: "100% Remote",
-    jobApplyUrl: "https://forms.gle/WoiNgB4w4pjLa1bf9",
-  },
-  {
-    jobName: "Call Center Representative",
-    jobType: "Sales",
-    jobCategory: "100% Remote",
-    jobApplyUrl: "https://forms.gle/WoiNgB4w4pjLa1bf9",
-  },
-  {
-    jobName: "Social Media Specialist",
-    jobType: "Marketing",
-    jobCategory: "100% Remote",
-    jobApplyUrl: "https://forms.gle/WoiNgB4w4pjLa1bf9",
-  },
-];
+let file = "../json/career.json";
 
-const divThree = document.querySelector("#jobs-list");
+xhr.open("GET", file, true);
+xhr.send();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    let data = JSON.parse(xhr.responseText);
+    let jobs = data;
 
-jobs.forEach((job) => {
-  const div = document.createElement("div");
-  div.setAttribute("class", "job");
+    const divThree = document.querySelector("#jobs-list");
 
-  const heading = document.createElement("h3");
-  heading.textContent = job.jobName;
+    jobs.forEach((job) => {
+      const div = document.createElement("div");
+      div.setAttribute("class", "job");
 
-  const discOne = document.createElement("p");
-  discOne.textContent = job.jobType;
+      const heading = document.createElement("h3");
+      heading.textContent = job.jobName;
 
-  const discTwo = document.createElement("p");
-  discTwo.textContent = job.jobCategory;
+      const discOne = document.createElement("p");
+      discOne.textContent = job.jobType;
 
-  const link = document.createElement("a");
-  link.setAttribute("href", job.jobApplyUrl);
-  link.setAttribute("target", "_blank");
-  link.setAttribute("class", "job-button btn btn-primary has-before has-after");
-  link.textContent = "Apply Now";
+      const discTwo = document.createElement("p");
+      discTwo.textContent = job.jobCategory;
 
-  div.append(heading);
-  div.append(discOne);
-  div.append(discTwo);
-  div.append(link);
+      const link = document.createElement("a");
+      link.setAttribute("href", job.jobApplyUrl);
+      link.setAttribute("target", "_blank");
+      link.setAttribute(
+        "class",
+        "job-button btn btn-primary has-before has-after"
+      );
+      link.textContent = "Apply Now";
 
-  divThree.append(div);
-});
+      div.append(heading);
+      div.append(discOne);
+      div.append(discTwo);
+      div.append(link);
+
+      divThree.append(div);
+    });
+  }
+};
--- js/ServicesAndProducts.js
+++ js/ServicesAndProducts.js
@@ -1,146 +1,104 @@
-const services = [
-  {
-    serviceName: "Network Design & Implementation",
-    serviceType: "IT Service",
-    serviceImage: "../images/network_service.jpg",
-    serviceDetails: "network.html",
-  },
-  {
-    serviceName: "Cloud Computing Solutions",
-    serviceType: "IT Service",
-    serviceImage: "../images/cloud_service.jpg",
-    serviceDetails: "cloud.html",
-  },
-
-  {
-    serviceName: "Data center Solutions",
-    serviceType: "IT Service",
-    serviceImage: "../images/data_service.jpg",
-    serviceDetails: "data.html",
-  },
-  {
-    serviceName: "ECP Projects",
-    serviceType: "Electrical Services",
-    serviceImage: "../images/ecp_service.jpg",
-    serviceDetails: "ecp.html",
-  },
-
-  {
-    serviceName: "Camera Installation",
-    serviceType: "Electrical Services",
-    serviceImage: "../images/camera_service.png",
-    serviceDetails: "camera.html",
-  },
-  {
-    serviceName: "Solar Installation",
-    serviceType: "Electrical Services",
-    serviceImage: "../images/solar_service.jpg",
-    serviceDetails: "solar.html",
-  },
-];
-
-const products = [
-  {
-    productName: "CCTV Cameras",
-    productDesc: "Eyes everywhere, security everywhere",
-    productImage: "../images/camera_product.jpg",
-  },
-  {
-    productName: "Solar Panels",
-    productDesc: "Harnessing the power of the sun, for a sustainable future",
-    productImage: "../images/solar_product.jpg",
-  },
-  {
-    productName: "Invertor",
-    productDesc: "Unlocking the power of energy efficiency",
-    productImage: "../images/inverter_product.png",
-  },
-  {
-    productName: "Breakers",
-    productDesc: "Ensure electrical safety, save life save property",
-    productImage: "../images/breaker_product.jpg",
-  },
-];
 
 // SETTING UP SERVICES
 
-const myDiv = document.querySelector("#services");
-services.forEach((service) => {
-  const list = document.createElement("li");
+let xhr = new XMLHttpRequest();
 
-  const Cartdiv = document.createElement("div");
-  Cartdiv.setAttribute("class", "project-card");
+let file = "../json/service.json";
+xhr.open("GET", file, true);
+xhr.send();
 
-  const figure = document.createElement("figure");
-  figure.setAttribute("class", "card-banner img-holder");
-  figure.setAttribute("style", "--width: 416; --height: 429");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    let data = JSON.parse(xhr.responseText);
+    let services = data;
+    const myDiv = document.querySelector("#services");
+    services.forEach((service) => {
+      const list = document.createElement("li");
 
-  const CartContent = document.createElement("div");
-  CartContent.setAttribute("class", "card-content");
+      const Cartdiv = document.createElement("div");
+      Cartdiv.setAttribute("class", "project-card");
 
-  const CartImage = document.createElement("img");
-  CartImage.setAttribute("src", service.serviceImage);
-  CartImage.setAttribute("width", "416");
-  CartImage.setAttribute("height", "429");
-  CartImage.setAttribute("loading", "lazy");
-  CartImage.setAttribute("class", "img-cover");
+      const figure = document.createElement("figure");
+      figure.setAttribute("class", "card-banner img-holder");
+      figure.setAttribute("style", "--width: 416; --height: 429");
 
-  const serviceHeading = document.createElement("h3");
-  serviceHeading.setAttribute("class", "h3");
+      const CartContent = document.createElement("div");
+      CartContent.setAttribute("class", "card-content");
 
-  const serviceType = document.createElement("a");
-  serviceType.setAttribute("class", "class-tag");
-  serviceType.textContent = service.serviceType;
+      const CartImage = document.createElement("img");
+      CartImage.setAttribute("src", service.serviceImage);
+      CartImage.setAttribute("width", "416");
+      CartImage.setAttribute("height", "429");
+      CartImage.setAttribute("loading", "lazy");
+      CartImage.setAttribute("class", "img-cover");
 
-  const serviceName = document.createElement("a");
-  serviceName.setAttribute("href", service.serviceDetails);
-  serviceName.setAttribute("class", "card-title");
-  serviceName.textContent = service.serviceName;
+      const serviceHeading = document.createElement("h3");
+      serviceHeading.setAttribute("class", "h3");
 
-  serviceHeading.appendChild(serviceName);
-  CartContent.appendChild(serviceHeading);
-  CartContent.appendChild(serviceType);
-  figure.appendChild(CartImage);
-  Cartdiv.appendChild(figure);
-  Cartdiv.appendChild(CartContent);
+      const serviceType = document.createElement("a");
+      serviceType.setAttribute("class", "class-tag");
+      serviceType.textContent = service.serviceType;
 
-  list.appendChild(Cartdiv);
-  myDiv.appendChild(list);
-});
+      const serviceName = document.createElement("a");
+      serviceName.setAttribute("href", service.serviceDetails);
+      serviceName.setAttribute("class", "card-title");
+      serviceName.textContent = service.serviceName;
 
-// SETTING UP PRODUCTS
+      serviceHeading.appendChild(serviceName);
+      CartContent.appendChild(serviceHeading);
+      CartContent.appendChild(serviceType);
+      figure.appendChild(CartImage);
+      Cartdiv.appendChild(figure);
+      Cartdiv.appendChild(CartContent);
 
-const myDivtwo = document.querySelector("#product-list");
-products.forEach((product) => {
-  const list = document.createElement("li");
-  const article = document.createElement("article");
-  article.setAttribute("class", "product-item");
+      list.appendChild(Cartdiv);
+      myDiv.appendChild(list);
+    });
+  }
+};
 
-  const div = document.createElement("div");
-  div.setAttribute("class", "product-image-container");
+// // SETTING UP PRODUCTS
+let xhrTwo = new XMLHttpRequest();
 
-  const image = document.createElement("img");
-  image.setAttribute("class", "project-image");
-  image.setAttribute("src", product.productImage);
+let fileTwo = "../json/product.json";
+xhrTwo.open("GET", fileTwo, true);
+xhrTwo.send();
 
-  const divInfo = document.createElement("div");
-  divInfo.setAttribute("class", "product-info");
+xhrTwo.onreadystatechange = function () {
+  if (xhrTwo.readyState === 4 && xhrTwo.status === 200) {
+    let data = JSON.parse(xhrTwo.responseText);
+    let products = data;
+    const myDivtwo = document.querySelector("#product-list");
+    products.forEach((product) => {
+      const list = document.createElement("li");
+      const article = document.createElement("article");
+      article.setAttribute("class", "product-item");
 
-  const Heading = document.createElement("h2");
-  Heading.textContent = product.productName;
+      const div = document.createElement("div");
+      div.setAttribute("class", "product-image-container");
 
-  const description = document.createElement("p");
-  description.textContent = product.productDesc;
+      const image = document.createElement("img");
+      image.setAttribute("class", "project-image");
+      image.setAttribute("src", product.productImage);
 
-  divInfo.appendChild(Heading);
-  divInfo.appendChild(description);
+      const divInfo = document.createElement("div");
+      divInfo.setAttribute("class", "product-info");
 
-  div.appendChild(image);
-  div.appendChild(divInfo);
-  article.appendChild(div);
-  list.appendChild(article);
-  myDivtwo.appendChild(list);
+      const Heading = document.createElement("h2");
+      Heading.textContent = product.productName;
 
-  console.log("end");
-});
+      const description = document.createElement("p");
+      description.textContent = product.productDesc;
 
+      divInfo.appendChild(Heading);
+      divInfo.appendChild(description);
+
+      div.appendChild(image);
+      div.appendChild(divInfo);
+      article.appendChild(div);
+      list.appendChild(article);
+      myDivtwo.appendChild(list);
+
+    });
+  }
+};
